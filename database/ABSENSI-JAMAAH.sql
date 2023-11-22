9eb24ef051f5e271ce19c94347a408e4c5dd834a
Ardi <93758608+Ardii0@users.noreply.github.com>
2023-11-22 08:20:39 +0700

    Update Database

--- database/ABSENSI-JAMAAH.sql
+++ database/ABSENSI-JAMAAH.sql
@@ -1,5 +1,5 @@
 /*
-SQLyog Professional v12.5.1 (64 bit)
+SQLyog Ultimate v12.5.1 (64 bit)
 MySQL - 10.4.27-MariaDB : Database - data_absensi
 *********************************************************************
 */
@@ -26,16 +26,10 @@ CREATE TABLE `akun` (
   `username` varchar(75) NOT NULL,
   `telepon` bigint(20) NOT NULL,
   `email` varchar(75) NOT NULL,
-  `nama_bisnis` varchar(75) DEFAULT NULL,
-  `alamat_bisnis` text DEFAULT NULL,
   `provinsi` varchar(75) DEFAULT NULL,
   `kota` varchar(75) DEFAULT NULL,
   `kecamatan` varchar(75) DEFAULT NULL,
   `desa` varchar(75) DEFAULT NULL,
-  `kode_pos` bigint(20) DEFAULT NULL,
-  `nama_npwp` varchar(75) DEFAULT NULL,
-  `nomor_npwp` varchar(20) DEFAULT NULL,
-  `foto_npwp` text DEFAULT NULL,
   `password` varchar(75) NOT NULL,
   `role` varchar(20) NOT NULL,
   `dibuat_pada` timestamp NULL DEFAULT current_timestamp(),
@@ -46,9 +40,9 @@ CREATE TABLE `akun` (
 
 /*Data for the table `akun` */
 
-insert  into `akun`(`id`,`nama_pengguna`,`username`,`telepon`,`email`,`nama_bisnis`,`alamat_bisnis`,`provinsi`,`kota`,`kecamatan`,`desa`,`kode_pos`,`nama_npwp`,`nomor_npwp`,`foto_npwp`,`password`,`role`,`dibuat_pada`,`diubah_pada`,`dihapus_pada`) values 
-(6410896282210479,'Admin POS','Admin',807952365,'admin@gmail.com','Pos Toko','Jl. Aceh pusat POS No. 980','JAWA TENGAH','KOTA SEMARANG','GUNUNG PATI','GUNUNGPATI',50789,'PT. POS Aplikasi','48.903.841.4-722.000',NULL,'$2y$12$eF1WkQt0DOMV7u8z6HbNzufgrSxC4TLDMW.yRk5hCmK/M5EETVrFi','Admin','2023-06-08 10:01:31','2023-09-18 08:29:17',NULL),
-(7392511986664469,NULL,'aa',0,'ada@ada',NULL,NULL,NULL,NULL,NULL,'',NULL,NULL,NULL,NULL,'$2y$12$2BgEydbUmSE7k3pjgIYc6O4UPrVuDx9D5RRaTLPJ0JRPb/c1VYnVu','Admin','2023-06-08 16:36:21','2023-06-09 14:24:24',NULL);
+insert  into `akun`(`id`,`nama_pengguna`,`username`,`telepon`,`email`,`provinsi`,`kota`,`kecamatan`,`desa`,`password`,`role`,`dibuat_pada`,`diubah_pada`,`dihapus_pada`) values 
+(6410896282210479,'Admin POS','Admin',807952365,'admin@gmail.com','JAWA TENGAH','KOTA SEMARANG','GUNUNG PATI','GUNUNGPATI','$2y$12$eF1WkQt0DOMV7u8z6HbNzufgrSxC4TLDMW.yRk5hCmK/M5EETVrFi','Admin','2023-06-08 10:01:31','2023-09-18 08:29:17',NULL),
+(7392511986664469,NULL,'aa',0,'ada@ada',NULL,NULL,NULL,'','$2y$12$2BgEydbUmSE7k3pjgIYc6O4UPrVuDx9D5RRaTLPJ0JRPb/c1VYnVu','Admin','2023-06-08 16:36:21','2023-06-09 14:24:24',NULL);
 
 /*Table structure for table `daftar_kegiatan` */
 
@@ -59,17 +53,17 @@ CREATE TABLE `daftar_kegiatan` (
   `nama_kegiatan` varchar(255) NOT NULL,
   `dibuat_pada` timestamp NOT NULL DEFAULT current_timestamp(),
   `diubah_pada` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `status_kegiatan` varchar(255) DEFAULT '1',
+  `status_kegiatan` tinyint(1) DEFAULT 1,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `daftar_kegiatan` */
 
 insert  into `daftar_kegiatan`(`id`,`nama_kegiatan`,`dibuat_pada`,`diubah_pada`,`status_kegiatan`) values 
-(1,'Sambung Kelompok','2023-11-03 23:17:30','2023-11-05 23:17:56','1'),
-(2,'Sambung Sub','2023-11-03 23:18:40','2023-11-05 23:21:35','1'),
-(3,'Sambung Pagi','2023-11-13 00:57:15','2023-11-13 00:57:15','1'),
-(4,'Susulan Text Amanatan','2023-11-16 15:43:31','2023-11-20 23:13:22','0');
+(1,'Sambung Kelompok','2023-11-03 23:17:30','2023-11-05 23:17:56',1),
+(2,'Sambung Sub','2023-11-03 23:18:40','2023-11-05 23:21:35',1),
+(3,'Sambung Pagi','2023-11-13 00:57:15','2023-11-22 08:15:40',1),
+(4,'Susulan Text Amanatan','2023-11-16 15:43:31','2023-11-20 23:13:22',0);
 
 /*Table structure for table `data_jamaah` */
 
@@ -79,12 +73,12 @@ CREATE TABLE `data_jamaah` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `code_unik` varchar(255) DEFAULT NULL,
   `nama_lengkap` varchar(255) NOT NULL,
-  `tgl_lahir` datetime NOT NULL,
+  `tgl_lahir` date NOT NULL,
   `tmpt_lahir` varchar(255) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `no_telepon` int(15) NOT NULL DEFAULT 62,
-  `kategori` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
+  `kategori` varchar(25) DEFAULT NULL,
+  `status` varchar(25) DEFAULT NULL,
   `dibuat_oleh` int(11) NOT NULL,
   `dibuat_pada` timestamp NOT NULL DEFAULT current_timestamp(),
   `diubah_pada` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -95,10 +89,10 @@ CREATE TABLE `data_jamaah` (
 /*Data for the table `data_jamaah` */
 
 insert  into `data_jamaah`(`id`,`code_unik`,`nama_lengkap`,`tgl_lahir`,`tmpt_lahir`,`alamat`,`no_telepon`,`kategori`,`status`,`dibuat_oleh`,`dibuat_pada`,`diubah_pada`,`status_data`) values 
-(1,'0','halo dek','1994-06-01 00:00:00','kaka','',62,'lansia','pendatang',0,'2023-11-16 16:20:49','2023-11-19 22:45:13',1),
-(2,NULL,'farid','2008-02-16 00:00:00','Demak','',62,'umum','pribumi',0,'2023-11-16 16:23:01','2023-11-16 16:24:57',1),
-(3,'20080218-ALYY','Alfy','2008-02-18 00:00:00','Kalimantan','',62,'umum','pendatang',0,'2023-11-16 16:24:40','2023-11-16 16:27:38',1),
-(4,'20050503-BF6Z','Irvanda','2005-05-03 00:00:00','Semarang','',62,'remaja','pribumi',0,'2023-11-16 16:30:16','2023-11-20 23:23:30',0);
+(1,'0','halo dek','1994-06-01','kaka','',62,'lansia','pendatang',0,'2023-11-16 16:20:49','2023-11-19 22:45:13',1),
+(2,NULL,'farid','2008-02-16','Demak','',62,'umum','pribumi',0,'2023-11-16 16:23:01','2023-11-16 16:24:57',1),
+(3,'20080218-ALYY','Alfy','2008-02-18','Kalimantan','',62,'umum','pendatang',0,'2023-11-16 16:24:40','2023-11-16 16:27:38',1),
+(4,'20050503-BF6Z','Irvanda','2005-05-03','Semarang','',62,'remaja','pribumi',0,'2023-11-16 16:30:16','2023-11-20 23:23:30',0);
 
 /*Table structure for table `data_masjid` */
 
@@ -110,7 +104,7 @@ CREATE TABLE `data_masjid` (
   `kyai_kelompok` varchar(255) NOT NULL,
   `penrobos` varchar(255) NOT NULL,
   `alamat_masjid` varchar(255) NOT NULL,
-  `status` int(11) DEFAULT 1,
+  `status` tinyint(1) DEFAULT 1,
   `dibuat_oleh` int(11) NOT NULL,
   `dibuat_pada` timestamp NOT NULL DEFAULT current_timestamp(),
   `diubah_pada` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
